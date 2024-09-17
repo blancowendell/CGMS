c9f2f04f0e4dfed527da3ff8d979d9f94e5b7454
WendellBlanco <wendellblanco78@gmail.com>
2024-09-17 17:57:00 +0800

    [COMPLETE QUESTION PROCESS]

---
+++ DB/CGMSDB20240917/cgms_master_choices.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cgms
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `master_choices`
+--
+
+DROP TABLE IF EXISTS `master_choices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `master_choices` (
+  `mc_id` int NOT NULL AUTO_INCREMENT,
+  `mc_name` varchar(100) NOT NULL,
+  `mc_choice_type` varchar(10) DEFAULT NULL,
+  `mc_personality_id` int NOT NULL,
+  `mc_question_id` int NOT NULL,
+  `mc_create_by` varchar(100) NOT NULL,
+  `mc_create_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`mc_id`),
+  KEY `mc_personality_id` (`mc_personality_id`),
+  KEY `master_choices_ibfk_2_idx` (`mc_question_id`),
+  CONSTRAINT `master_choices_ibfk_1` FOREIGN KEY (`mc_personality_id`) REFERENCES `personality_type` (`pt_type_id`),
+  CONSTRAINT `master_choices_ibfk_2` FOREIGN KEY (`mc_question_id`) REFERENCES `questions` (`q_question_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `master_choices`
+--
+
+LOCK TABLES `master_choices` WRITE;
+/*!40000 ALTER TABLE `master_choices` DISABLE KEYS */;
+INSERT INTO `master_choices` VALUES (3,'Mag lulu na lang ako','no',1,4,'Tanggol Dimaguiba','2024-09-17 09:54:00'),(4,'Sa bahay lang','yes',2,4,'Tanggol Dimaguiba','2024-09-17 09:54:00'),(5,'Alone  ','no',3,5,'Tanggol Dimaguiba','2024-09-17 11:00:00'),(6,'Alone  ','yes',1,5,'Tanggol Dimaguiba','2024-09-17 11:00:00'),(7,'Alone  ','yes',1,6,'Tanggol Dimaguiba','2024-09-17 17:38:00'),(8,'With others','no',2,6,'Tanggol Dimaguiba','2024-09-17 17:38:00');
+/*!40000 ALTER TABLE `master_choices` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-17 17:55:52
--- routes/questions.js
+++ routes/questions.js
@@ -38,8 +38,8 @@ router.get("/load", (req, res) => {
   try {
     let sql = `SELECT 
     q_question_id,
-    a_assessment_name as q_assessment,
-    q_question_text,
+    q_question_name,
+    a_assessment_name as q_assessment_name, 
     q_question_type
     FROM questions
     INNER JOIN assessments ON questions.q_assessment_id = a_assessment_id`;
@@ -67,72 +67,69 @@ router.get("/load", (req, res) => {
 
 router.post("/save", (req, res) => {
   try {
-    const { assessment_id, question_text, ifyes, ifno, ifyesname, ifnoname } = req.body;
+    const { assessment_id, question_text, ifyes, ifno, ifyesname, ifnoname, question_name } = req.body;
     let created_by = req.session.fullname;
     let created_date = GetCurrentDatetime();
-
+    let school_id = req.session.schoolid;
+    let ifnotype = 'no';
+    let ifyestype = 'yes';
     let insertQuestions = InsertStatement("questions", "q", [
+      "question_name",
+      "school_id",
       "assessment_id",
       "question_text",
       "question_type",
-      "created_by",
-      "created_date",
     ]);
-
     let insertChoices = InsertStatement("master_choices", "mc", [
       "name",
+      "choice_type",
       "personality_id",
-      "created_by",
-      "created_date",
+      "question_id", 
+      "create_by",
+      "create_date",
     ]);
 
-    let dataChoicesifYes = [
-      [
-        ifyesname,
-        ifyes,
-        created_by,
-        created_date,
-      ],
-    ];
-
-    let dataChoicesifNo = [
-      [
-        ifnoname,
-        ifno,
-        created_by,
-        created_date,
-      ],
-    ];
-
     let dataQuestions = [
-      [
-        assessment_id,
-        question_text,
-        ifyes,
-        ifno,
-        created_by,
-        created_date,
-      ],
+      [question_name ,school_id, assessment_id, question_text, "Multiple Choice",],
     ];
 
     let checkStatement = SelectStatement(
-      "select * from questions where q_assessment_id = ? and q_question_text = ? and q_question_type = ?",
-      [assessment_id, question_text, ifyes]
+      "select * from questions where q_assessment_id = ? and q_question_name = ?",
+      [assessment_id, question_name]
     );
 
     Check(checkStatement)
       .then((result) => {
-        console.log(result);
-        if (result != 0) {
+        if (result.length !== 0) {
           return res.json(JsonWarningResponse(MessageStatus.EXIST));
         } else {
-          InsertTable(sql, data, (err, result) => {
+          InsertTable(insertQuestions, dataQuestions, (err, result) => {
             if (err) {
               console.log(err);
-              res.json(JsonErrorResponse(err));
+              return res.json(JsonErrorResponse(err));
             }
 
-            res.json(JsonSuccess());
+            let insertedQuestionId = result[0].id;
+            let dataChoicesifYes = [
+              [ifyesname, ifyestype, ifyes, insertedQuestionId, created_by, created_date],
+            ];
+
+            let dataChoicesifNo = [
+              [ifnoname, ifnotype, ifno, insertedQuestionId, created_by, created_date],
+            ];
+            InsertTable(insertChoices, dataChoicesifYes, (err, result) => {
+              if (err) {
+                console.log(err);
+                return res.json(JsonErrorResponse(err));
+              }
+              InsertTable(insertChoices, dataChoicesifNo, (err, result) => {
+                if (err) {
+                  console.log(err);
+                  return res.json(JsonErrorResponse(err));
+                }
+                return res.json(JsonSuccess());
+              });
+            });
           });
         }
       })
@@ -145,6 +142,248 @@ router.post("/save", (req, res) => {
     res.json(JsonErrorResponse(error));
   }
 });
+
+router.post("/getchoicesidyes", (req, res) => {
+  try {
+    let question_id = req.body.question_id;
+    let sql = `SELECT mc_id 
+    FROM master_choices 
+    WHERE mc_question_id = '${question_id}'
+    AND mc_choice_type = 'yes'`;
+
+    Select(sql, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.json(JsonErrorResponse(err));
+      }
+
+      if (result != 0) {
+        let data = DataModeling(result, "mc_");
+
+        console.log(data);
+        res.json(JsonDataResponse(data));
+      } else {
+        res.json(JsonDataResponse(result));
+      }
+    });
+  } catch (error) {
+    res.json(JsonErrorResponse(error));
+  }
+});
+
+router.post("/getchoicesidno", (req, res) => {
+  try {
+    let question_id = req.body.question_id;
+    let sql = `SELECT mc_id 
+    FROM master_choices 
+    WHERE mc_question_id = '${question_id}'
+    AND mc_choice_type = 'no'`;
+
+    Select(sql, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.json(JsonErrorResponse(err));
+      }
+
+      if (result != 0) {
+        let data = DataModeling(result, "mc_");
+
+        console.log(data);
+        res.json(JsonDataResponse(data));
+      } else {
+        res.json(JsonDataResponse(result));
+      }
+    });
+  } catch (error) {
+    res.json(JsonErrorResponse(error));
+  }
+});
+
+
+router.post("/getquestion", (req, res) => {
+  try {
+    let question_id = req.body.question_id;
+    let sql = `  SELECT 
+    q_question_name,
+    q_assessment_id,
+    q_question_text,
+    mc_name as q_choice_name,
+    mc_personality_id as q_personality_id,
+    mc_choice_type as q_choice_type
+    FROM questions
+    INNER JOIN master_choices ON questions.q_question_id = mc_question_id
+    WHERE q_question_id = '${question_id}'`;
+
+    Select(sql, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.json(JsonErrorResponse(err));
+      }
+
+      if (result != 0) {
+        let data = DataModeling(result, "q_");
+
+        console.log(data);
+        res.json(JsonDataResponse(data));
+      } else {
+        res.json(JsonDataResponse(result));
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.json(JsonErrorResponse(error));
+  }
+});
+
+router.put("/edit", async (req, res) => {
+  try {
+    const {
+      question_id,
+      assessment_id,
+      question_name,
+      question_text,
+      ifyes,
+      ifno,
+      ifyesname,
+      ifnoname,
+      globalYesChoiceId,
+      globalNoChoiceId
+    } = req.body;
+
+    let questionData = [];
+    let questionColumns = [];
+    let questionArguments = [];
+    
+    if (assessment_id) {
+      questionData.push(assessment_id);
+      questionColumns.push("assessment_id");
+    }
+
+    if (question_name) {
+      questionData.push(question_name);
+      questionColumns.push("question_name");
+    }
+
+    if (question_text) {
+      questionData.push(question_text);
+      questionColumns.push("question_text");
+    }
+
+    if (question_id) {
+      questionData.push(question_id);
+      questionArguments.push("question_id");
+    }
+
+    let updateQuestionStatement = UpdateStatement(
+      "questions",
+      "q",
+      questionColumns,
+      questionArguments
+    );
+
+    let checkStatement = SelectStatement(
+      "SELECT * FROM master_choices WHERE mc_personality_id = ? AND mc_question_id = ? ",
+      [ifyes, question_id]
+    );
+
+    let duplicate = await Check(checkStatement);
+
+    if (duplicate.length > 0) {
+      return res.json(JsonWarningResponse("exist"));
+    }
+
+    await Update(updateQuestionStatement, questionData, (err, result) => {
+      if (err) {
+        console.error("Error updating questions: ", err);
+        return res.json(JsonErrorResponse(err));
+      }
+
+      console.log("Questions updated:", result);
+
+        let choiceDataYes = [];
+        let choiceColumnsYes = [];
+        let choiceArgumentsYes = [];
+
+        if (ifyesname) {
+          choiceDataYes.push(ifyesname);
+          choiceColumnsYes.push("name");
+        }
+
+        if (ifyes) {
+          choiceDataYes.push(ifyes);
+          choiceColumnsYes.push("personality_id");
+        }
+
+        if (globalYesChoiceId) {
+          choiceDataYes.push(globalYesChoiceId);
+          choiceArgumentsYes.push("id");
+        }
+
+        let updateChoiceStatementYes = UpdateStatement(
+          "master_choices",
+          "mc",
+          choiceColumnsYes,
+          choiceArgumentsYes
+        );
+
+        console.log("Generated SQL Query for 'If Yes':", updateChoiceStatementYes);
+        console.log("Data for 'If Yes':", choiceDataYes);
+
+         Update(updateChoiceStatementYes, choiceDataYes, (err, result) => {
+          if (err) {
+            console.error("Error updating 'If Yes' choices: ", err);
+            return res.json(JsonErrorResponse(err));
+          }
+          console.log("'If Yes' Choices updated:", result);
+        });
+
+        let choiceDataNo = [];
+        let choiceColumnsNo = [];
+        let choiceArgumentsNo = [];
+
+        if (ifnoname) {
+          choiceDataNo.push(ifnoname);
+          choiceColumnsNo.push("name");
+        }
+
+        if (ifno) {
+          choiceDataNo.push(ifno);
+          choiceColumnsNo.push("personality_id");
+        }
+
+        if (globalNoChoiceId) {
+          choiceDataNo.push(globalNoChoiceId);
+          choiceArgumentsNo.push("id");
+        }
+
+        let updateChoiceStatementNo = UpdateStatement(
+          "master_choices",
+          "mc",
+          choiceColumnsNo,
+          choiceArgumentsNo
+        );
+
+        console.log("Generated SQL Query for 'If No':", updateChoiceStatementNo);
+        console.log("Data for 'If No':", choiceDataNo);
+
+       Update(updateChoiceStatementNo, choiceDataNo, (err, result) => {
+          if (err) {
+            console.error("Error updating 'If No' choices: ", err);
+            return res.json(JsonErrorResponse(err));
+          }
+          console.log("'If No' Choices updated:", result);
+        });
+      res.json(JsonSuccess("success"));
+    });
+  } catch (error) {
+    console.log(error);
+    res.json(JsonErrorResponse(error));
+  }
+});
+
+
+
+
 
 
 //#region FUNCTION
