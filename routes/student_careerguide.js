9922ee31711639c9794229d53616f0ed4eeaa2a0
blancowendell <blanco.wendell15@gmail.com>
2024-09-20 16:40:19 +0800

    [ADD STUDENT ANSWER RESULT]

--- routes/student_careerguide.js
+++ routes/student_careerguide.js
@@ -125,6 +125,64 @@ router.post("/submitanswers", function (req, res) {
 });
 
 
+router.get("/viewresult", function (req, res) {
+  try {
+    let school_id = req.session.schoolid;
+    let student_id = req.session.studentid;
+
+    let sql  = `
+        SELECT 
+        pt.pt_type_id AS q_personality_type_id,
+        pt.pt_code AS q_personality_code,
+        pt.pt_description AS q_personality_description,
+        pt.pt_strands_id AS q_strand_ids,
+        COUNT(pt.pt_type_id) AS q_personality_count,  -- Add this back for counting personality types
+        GROUP_CONCAT(DISTINCT asn.as_name) AS q_strand_names,
+        GROUP_CONCAT(DISTINCT asn.as_course_description) AS q_course_descriptions,
+        GROUP_CONCAT(DISTINCT asn.as_job_description) AS q_job_descriptions
+    FROM 
+        student_answers sa
+    JOIN 
+        master_choices mc ON sa.sa_choice = mc.mc_id
+    JOIN 
+        personality_type pt ON mc.mc_personality_id = pt.pt_type_id
+    LEFT JOIN 
+        academic_strands asn ON JSON_CONTAINS(pt.pt_strands_id, CONCAT('"', asn.as_id, '"')) -- Matching the strands in JSON
+    WHERE 
+        sa.sa_student_id = '${student_id}'
+        AND sa.sa_school_id = pt.pt_school_id
+        AND asn.as_school_id = '${school_id}'
+    GROUP BY 
+        pt.pt_type_id, pt.pt_code, pt.pt_description, pt.pt_strands_id
+    ORDER BY 
+        q_personality_count DESC  -- Now correctly reference the alias
+    LIMIT 1`;
+
+    console.log(sql);
+    
+
+    Select(sql, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.json(JsonErrorResponse(err));
+      }
+
+      if (result != 0) {
+        let data = DataModeling(result, "q_");
+
+        console.log(data);
+        res.json(JsonDataResponse(data));
+      } else {
+        res.json(JsonDataResponse(result));
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.json(JsonErrorResponse(error))
+  }
+});
+
+
 //#region FUNCTION
 function Check(sql) {
   return new Promise((resolve, reject) => {
